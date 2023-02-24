59dc9aa9085657119005f4bba07b7d37d1807359
James <jamesnixonsteel@gmail.com>
2023-02-24 16:15:47 +0000

    resolved feedback

--- controllers/patchVote.js
+++ controllers/patchVote.js
@@ -3,7 +3,7 @@ const insertVote = require("../models/insertVote");
 const patchVote = (req, res, next) => {
   const articleID = req.params.article_id;
   const vote = req.body.inc_votes;
-  console.log("you have arrived");
+
   insertVote(articleID, vote)
     .then((article) => {
       res.status(200).send({ article });
