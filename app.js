c0aec5fe635a9aad5614987a621e19ed42d3b193
James <jamesnixonsteel@gmail.com>
2023-03-13 10:40:49 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+app.use(cors());
 const getTopics = require("./controllers/getTopics.js");
 const getArticles = require("./controllers/getArticles.js");
 const getArticleByID = require("./controllers/getArticleByID.js");
